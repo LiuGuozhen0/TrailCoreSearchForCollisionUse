edec316e1e1dc71f817b389c525879416ec31fcf
Liu Guozhen <liuguozhen5@gmail.com>
2018-02-21 18:14:04 +0800

    Reorganize code.
    Previously, I wrote a bunch of randomly listed codes at the main file. I relocate them to the "testtools.cpp" and "testtools.h". Most of them are for test usage.

---
+++ testtools.h
@@ -0,0 +1,39 @@
+/*
+ * This text is created to implement some algorithms when necessary.
+ */
+
+ #ifndef _TESTTOOLS_
+ #define _TESTTOOLS_
+
+ #include <cstdlib>
+ #include <fstream>
+ #include <iostream>
+ #include <sstream>
+ #include <string.h>
+ #include "duplex.h"
+ #include "Keccak.h"
+ #include "KeccakCrunchyContest.h"
+ #include "Keccak-f25LUT.h"
+ #include "Keccak-fCodeGen.h"
+ #include "Keccak-fDCEquations.h"
+ #include "Keccak-fDCLC.h"
+ #include "Keccak-fEquations.h"
+ #include "Keccak-fPropagation.h"
+ #include "Keccak-fTrailExtension.h"
+ #include "Keccak-fTrails.h"
+ #include "Keyakv2-test.h"
+ #include "Keccak-fTrailCoreInKernelAtC.h"
+ #include "Keccak-fDisplay.h"
+ #include "compareTrails.h"
+ #include "misc.h"
+
+ #include <iostream>
+ //#include "efficiency.h"
+ #include <sys/time.h>
+ #include <time.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <string.h>//memset()
+
+ using namespace std;
